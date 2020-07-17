dc4109eb3e147970bc3a8b9b7688f25235728293
Eze <ezequieldominguezcp11@gmail.com>
2020-07-17 12:13:17 -0300

    Updates

---
+++ precache-manifest.a19677049f318ebb14d9bf5d7a44f83b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc18d9613fca0f94d35ea66c50d86451",
+    "url": "/Rick-Morty-App/index.html"
+  },
+  {
+    "revision": "23d08c5460357776a3b2",
+    "url": "/Rick-Morty-App/static/css/main.0c39824b.chunk.css"
+  },
+  {
+    "revision": "f4664f0b9384e26d7af0",
+    "url": "/Rick-Morty-App/static/js/2.e5d38265.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rick-Morty-App/static/js/2.e5d38265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d08c5460357776a3b2",
+    "url": "/Rick-Morty-App/static/js/main.2f15a11a.chunk.js"
+  },
+  {
+    "revision": "2d31e05bfe11b7a70410",
+    "url": "/Rick-Morty-App/static/js/runtime-main.57793271.js"
+  }
+]);
